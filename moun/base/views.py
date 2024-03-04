56046b7124e3bc208f9511360e8d1c27d2eb91f7
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-03-04 15:55:12 -0500

    set up the base urls + links base urls to core app

--- moun/base/views.py
+++ moun/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Hello Moun')
+
+
+def chanm(request):
+    return HttpResponse('Chanm 16')
--- moun/moun/urls.py
+++ moun/moun/urls.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
 
-def home(request):
-    return HttpResponse('Hello Moun')
-
-
-def chanm(request):
-    return HttpResponse('Chanm 16')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('chanm/', chanm),
+    path('', include('base.urls')),
+    
 ]
 
