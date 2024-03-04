66f568239850fca8a6e1620d5288559b5f7557d3
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-03-04 15:45:49 -0500

    link base to main_project + set home_and_cham func_and_url

--- moun/moun/urls.py
+++ moun/moun/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Hello Moun')
+
+
+def chanm(request):
+    return HttpResponse('Chanm 16')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('chanm/', chanm),
 ]
 
