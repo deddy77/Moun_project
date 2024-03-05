a5cb56ea50dd894872912dc8c0c6850d74c0fabc
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-03-05 10:08:02 -0500

    redirect user to home

--- moun/base/views.py
+++ moun/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 # Create your views here.
@@ -33,5 +33,7 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('home')
+
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
