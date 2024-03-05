0d7626d1b277c93ae74ccb3a2d11bc389c0c86c8
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-03-05 08:40:26 -0500

    create message model

--- moun/base/models.py
+++ moun/base/models.py
@@ -14,3 +14,18 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Message(models.Model):
+   # user = models.CharField(max_length=200)
+    #value = models.TextField()
+    #date = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+        return self.value
