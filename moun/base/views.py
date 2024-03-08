25a06f58596a66ef598c3f70c456a1164bf7399d
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-03-08 01:26:44 -0500

    apply template to form

--- moun/base/views.py
+++ moun/base/views.py
@@ -122,19 +122,30 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name =request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        #form = RoomForm(request.POST)
 
-    context = {'form': form}
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            #created = created,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+
+        )
+        return redirect('home')
+
+    context = {'form': form, 'topics': topics,}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not allowed here!')
@@ -147,7 +158,10 @@ def updateRoom(request, pk):
             room.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form,
+               'topics': topics,
+               'room': room,
+               }
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
