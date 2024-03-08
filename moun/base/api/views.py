123926b825a434ebea54564979c85a8b1c77392b
Wood Eddy Donald Jean Julien <139917162+deddy77@users.noreply.github.com>
2024-03-08 11:03:58 -0500

    fixing env and django rest framework

--- moun/base/api/views.py
+++ moun/base/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
 
 
 @api_view(['GET'])
@@ -10,3 +11,8 @@ def getRoutes(request):
         'GET /api/rooms/:id'
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    return Response(room)
